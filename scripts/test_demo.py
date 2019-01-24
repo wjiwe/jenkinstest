e378fa1a640c64468ab196e957656988f8611fdc
wangjw <395793249@qq.com>
2019-01-24 18:44:45 +0800

    v1.0

---
+++ scripts/test_demo.py
@@ -0,0 +1,14 @@
+import allure
+
+
+class TestDemo:
+
+    #@allure.step(title="登录测试流程")
+    def test_01(self):
+        #allure.attach("登录", "输入用户名")
+        print("helloworld!")
+        assert 1
+
+    def test_02(self):
+        print("helloworld!")
+        assert 0
