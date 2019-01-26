5ffb186f6d4353884b0816c2dc2f68bc18f77153
wjiwe <46986676+wjiwe@users.noreply.github.com>
2019-01-26 12:23:55 +0800

    Update test_demo.py

--- scripts/test_demo.py
+++ scripts/test_demo.py
@@ -3,12 +3,12 @@ import allure
 
 class TestDemo:
 
-    #@allure.step(title="登录测试流程")
+    @allure.step(title="登录测试流程")
     def test_01(self):
-        #allure.attach("登录", "输入用户名")
+        allure.attach("登录", "输入用户名")
         print("helloworld!")
         assert 1
 
     def test_02(self):
         print("helloworld!")
-        assert 1
+        assert 0
